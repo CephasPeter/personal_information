4035d139ca1400586f32a2fa3a0f89ede186b7ed
Cephas Arowolo <cephasarowolo@gmail.com>
2023-09-08 02:21:23 +0100

    done

--- README.md
+++ README.md
@@ -8,5 +8,9 @@ This project is the first task for the HNG-X x I4G internship. It is a simple fl
 - Run the project
 
 ## Demo
+- [Click here](https://appetize.io/app/l6fyfo2kxyjbn46qzgwqjnatia?device=pixel6&osVersion=12.0&scale=75) to view the demo video on Appetize.io.
+
+## Note
+- Running on android 11 and below on Appetize.io will result in a crash. Try using a real device or running on android 12 and above.
 
 
--- android/app/src/main/kotlin/com/hng/personal/info/personal_information/MainActivity.kt
+++ android/app/src/main/kotlin/com/hng/personal/info/personal_information/MainActivity.kt
@@ -1,6 +1,18 @@
 package com.hng.personal.info.personal_information
 
+import android.os.Build
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
+import androidx.core.view.WindowCompat
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            splashScreen.setOnExitAnimationListener { splashScreenView -> splashScreenView.remove() }
+        }
+        super.onCreate(savedInstanceState)
+    }
 }
